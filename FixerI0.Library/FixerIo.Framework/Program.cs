292e27e2f1823dd4e5aa23586c38d02c73a006e5
Admin <Admin@DESKTOP-NG04V9R>
2019-12-07 14:51:15 +0400

    Add fixer io client.

---
+++ FixerI0.Library/FixerIo.Framework/Program.cs
@@ -0,0 +1,22 @@
+﻿using FixerI0.Library;
+using System;
+
+namespace FixerIo.Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your API Access Key: ");
+            string accessKey = Console.ReadLine();
+            var fixerClient = new FixerIoClient(accessKey);
+            Currency response = fixerClient.GetLatest(Rates.RUB);
+            //Currency response = fixerClient.GetLatest(Rates.AED,Rates.USD);
+            //Currency response = fixerClient.Get(DateTime.Parse("2013,03,16"));
+            //Currency response = fixerClient.Get(DateTime.Parse("2013,03,16"), Rates.AED, Rates.USD, Rates.RUB);
+
+
+            Console.ReadKey();
+        }
+    }
+}
